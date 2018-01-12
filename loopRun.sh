a4bceae838ef093e31aa92e8d78ab305a85b469f
Shule Yu <ysl6.626@gmail.com>
2018-01-12 15:42:01 -0700

    sc

---
+++ loopRun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for SNR in 0 0.05 0.1 0.15 0.2 0.25 0.3
+do
+	echo "<NoiseLevel>  ${SNR}" > tmpfile_$$
+	awk 'NR>1 {print $0}' INFILE >> tmpfile_$$
+	mv tmpfile_$$ INFILE
+
+	./Run.sh
+
+done
+
+
+exit 0
