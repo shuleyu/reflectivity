c7e945688284caad56d940122ca4ba84544a88c4
Shule Yu <ysl6.626@gmail.com>
2018-03-06 13:26:29 -0700

    sc

--- LIST.sh
+++ LIST.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-${SRCDIR}/a01.GenerateModels.sh
-# ${SRCDIR}/a02.RunCRFL.sh
-# ${SRCDIR}/a03_1.PostProcess.sh
+# ${SRCDIR}/a01.GenerateModels.sh
+${SRCDIR}/a02.RunCRFL.sh
+${SRCDIR}/a03_1.PostProcess.sh
 # ${SRCDIR}/a03_2.PostProcess_AddNoise.sh
 # ${SRCDIR}/a03_3.AddDataNoise.sh
 
